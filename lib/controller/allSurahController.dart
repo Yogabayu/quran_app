bc10c9c96b4459709a9048cec2abe1bae842116c
Yogabayu <yogabayusbi@gmail.com>
2023-01-19 13:42:18 +0700

    update flow

--- lib/controller/allSurahController.dart
+++ lib/controller/allSurahController.dart
@@ -2,34 +2,17 @@ import 'dart:io';
 import 'dart:async';
 import 'dart:convert';
 import 'package:get/get.dart';
-import 'package:http/io_client.dart';
 import 'package:quran_app/model/allSurahModel.dart';
-// import 'package:http/http.dart' as http;
+import 'package:http/http.dart' as http;
 
 class AllSurahController extends GetxController {
-  Future<AllSurahModel> getData() async {
+  Future<List<AllSurahModel>> fetchSurah() async {
     try {
-      Future.delayed(Duration(seconds: 3));
-      final ioc = new HttpClient();
-      ioc.badCertificateCallback =
-          (X509Certificate cert, String host, int port) => true;
-      final http = new IOClient(ioc);
-      final response = await http.get(
-        Uri.parse(
-          "https://quran-api.santrikoding.com/api/surah/",
-        ),
-      );
-      // print(response.body);
-      if (response.statusCode == 200) {
-        final json = jsonDecode(response.body);
-        AllSurahModel resp =
-            AllSurahModel.fromJson(json as Map<String, dynamic>);
-        return resp;
-        // return AllSurahModel.fromJson(jsonDecode(response.body));
-      } else {
-        // print(response.statusCode);
-        throw 'Gagal mengambil data, cek kembali inputan anda';
-      }
+      var response = await http
+          .get(Uri.parse("https://quran-api.santrikoding.com/api/surah/"));
+      return (json.decode(response.body) as List)
+          .map((e) => AllSurahModel.fromJson(e))
+          .toList();
     } on SocketException {
       throw 'Mohon Cek internet anda';
     } on TimeoutException {
